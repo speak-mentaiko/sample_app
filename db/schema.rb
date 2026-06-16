b25cf63ff3bf638aaacfad77d03062f2f311bd9c
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-16 15:22:12 +0900

    fix: メールアドレスのバリデーションを詳しく

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_16_043651) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_16_060542) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
