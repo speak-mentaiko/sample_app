523acaf3ef8477b55da7574a72e762fcec7a39e5
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-16 16:07:52 +0900

    feat: Userモデルにpasswordの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
     format: { with: URI::MailTo::EMAIL_REGEXP },
     uniqueness: true
   )
+  validates :password, presence: true, length: { minimum: 8 }
+
+  has_secure_password
 end
