d11df541dc10cc503564863afa4bc54c4c5bc49e
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-18 10:44:33 +0900

    chore: ログインの実装

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,10 +4,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     assert_response :success
-    #assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
     assert_response :unprocessable_entity
-    #assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
