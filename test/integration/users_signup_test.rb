cbe8cb0b40835a0cfcb58c5c00a66cd8e93da971
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-22 10:38:34 +0900

    chore: ユーザ登録時にログイン

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -34,5 +34,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,9 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
 end
