da697fea93f7d6caa0ad823b3b67ae0f1b867ffa
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-22 11:59:06 +0900

    chore: テストの分割

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+end
 
-  test "login with invalid information" do
+class InvalidPasswordTest < UsersLogin
+  test "login path" do
     get login_path
     assert_response :success
+  end
+
+  test "login with invalid information" do
     post login_path, params: { session: { email: "", password: "" } }
     assert_not is_logged_in?
     assert_response :unprocessable_entity
@@ -17,8 +22,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with invalid email" do
-    get login_path
-    assert_response :success
     post login_path, params: { session: { email: "", password: "password" } }
     assert_not is_logged_in?
     assert_response :unprocessable_entity
@@ -28,8 +31,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with invalid password" do
-    get login_path
-    assert_response :success
     post login_path, params: { session: { email: @user.email, password: "" } }
     assert_not is_logged_in?
     assert_response :unprocessable_entity
@@ -37,23 +38,43 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+end
 
-  test "login with valid information" do
+class ValidLoginTest < UsersLogin
+  def setup
+    super
     post login_path, params: {
       session: { email:    @user.email, password: "password" }
     }
+  end
+
+  test "valid login" do
     assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_response :success
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+end
 
+class LogoutTest < UsersLogin
+  def setup
+    super
     delete logout_path
+  end
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+  end
+
+  test "login with valid information" do
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
