712a675182f46ee544f97a81f75b69ac408a2046
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-23 16:14:07 +0900

    chore: HEADリクエストに対応

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -56,6 +56,6 @@ module SessionsHelper
 
   # アクセスしようとしたURLを保存する
   def store_location
-    session[:forwarding_url] = request.original_url if request.get?
+    session[:forwarding_url] = request.original_url if request.get? || request.head?
   end
 end
