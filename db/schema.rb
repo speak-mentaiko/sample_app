8d94b71afbff1aa632f3918d04bf5238424a52b8
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-25 11:31:23 +0900

    activateコントローラの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_24_054918) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_25_021256) do
   create_table "users", force: :cascade do |t|
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "activation_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email"
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ User.create!(
   email: "example@railstutorial.org",
   password: "hogehoge",
   password_confirmation: "hogehoge",
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 # 追加のユーザーをまとめて生成する
@@ -16,6 +18,8 @@ User.create!(
     name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
