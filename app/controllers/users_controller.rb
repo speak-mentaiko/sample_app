85c5285f59b2fc76a92975358e850107053bcb80
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-25 15:00:30 +0900

    chore: createアクションの更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render "new", status: :unprocessable_entity
     end
@@ -50,7 +49,9 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(
+        :name, :email, :password, :password_confirmation
+      )
     end
 
     # ログイン済みユーザーかどうか確認
