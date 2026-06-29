3ad30909ad13c70bca55a0b0093bc9100dd1e252
speak-mentaiko <1717miyawaki@gmail.com>
2026-06-29 15:26:01 +0900

    feat: ユーザーページにポスト一覧を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url, status: :see_other and return unless @user.activated?
+    @microposts = @user.microposts.page(params[:page]).per(30)
   end
 
   def new
