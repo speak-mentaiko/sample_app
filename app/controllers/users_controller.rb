5c6a49466408b8e047668fa2c0cd9bfd4cb98b91
speak-mentaiko <1717miyawaki@gmail.com>
2026-07-13 16:08:27 +0900

    lint&update gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,14 +54,14 @@ class UsersController < ApplicationController
     @title = "Following"
     @user  = User.find(params[:id])
     @users = @user.following.page(params[:page])
-    render 'show_follow'
+    render "show_follow"
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
     @users = @user.followers.page(params[:page])
-    render 'show_follow'
+    render "show_follow"
   end
 
   private
